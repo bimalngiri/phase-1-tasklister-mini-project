5c9369ac77c979853cbdcda0f84cd9b1617127c5
Bimal Giri <bimalbishu2040@gmail.com>
2023-04-16 20:17:59 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const addTask = () => {
+      li.innerText = `${newTask} `;
+      btn.innerText = "x";
+      li.appendChild(btn);
+      ul.appendChild(li);
+    }
+
+    const deleteTask = (e) => {
+      e.target.parentNode.remove();
+    }
+
+    const clearInput = () => {
+      taskInput.value = "";
+    }
+
+    const taskInput = document.getElementById("new-task-description");
+    const newTask = taskInput.value;
+    const ul = document.getElementById("tasks");
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.addEventListener('click', deleteTask);
+    addTask();
+    clearInput();
+  });
 });
